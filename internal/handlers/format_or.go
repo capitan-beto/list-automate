e970587191c6a5fe16c28c7865447566c33d2b04
Otroletravaladna <crnana98@gmail.com>
2024-08-11 17:01:52 -0300

    feat: add functions

--- internal/handlers/format_or.go
+++ internal/handlers/format_or.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"strconv"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/xuri/excelize/v2"
@@ -61,19 +62,53 @@ func HandleOR() {
 				log.Error(err)
 			}
 
+			//obtener valor calculado de celda BUSCARV
+
 			vlupCells, err := f.CalcCellValue(sheet, vlupWriteCoord)
 			if err != nil {
 				log.Error(err)
+				continue
 			}
+
+			//convertir valor a int
 
 			val, err := strconv.Atoi(vlupCells)
 			if err != nil {
 				log.Error(err)
 			}
 
-			fmt.Println(val * 2)
+			//sumar porcentaje de gananacia
+
+			percentage := (val / 100) * 10
+			newPrice := val + percentage
+
+			//obtener coordenadas de celdas a aplicar porcentaje de ganancias
+
+			newPriceCoords, err := excelize.CoordinatesToCellName(5, i+1)
+			if err != nil {
+				log.Error(err)
+				return
+			}
+
+			//escribir nuevo valor
+
+			if err = f.SetCellFormula(sheet, newPriceCoords, fmt.Sprintf("=DOLLAR(%d, 2)", newPrice)); err != nil {
+				log.Error(err)
+				return
+			}
 		}
 
+	}
+
+	now := time.Now()
+
+	if err = f.SetCellValue(sheet, "E2", now.Format("02/01/2006")); err != nil {
+		log.Error(err)
+	}
+
+	if err := f.RemoveCol(sheet, "D"); err != nil {
+		log.Error(err)
+		return
 	}
 
 	if err = f.SaveAs("NewList.xlsx"); err != nil {
