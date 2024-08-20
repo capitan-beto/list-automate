cb4469480ed0111a8adad2f7e2e5f846431bd77e
Otroletravaladna <crnana98@gmail.com>
2024-08-19 23:32:52 -0300

    set env files, dotenv

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,11 +4,19 @@ import (
 	"flag"
 
 	"cmd/api/main.go/internal/handlers"
+
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
 )
 
 var list = flag.String("list", "none", "list requested for processing")
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Error("Error loading .env file")
+	}
+
 	flag.Parse()
 	handlers.Handler(*list)
 }
