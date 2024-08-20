a08268009d51a21bb19a754e214c87c6a79ff7a4
Otroletravaladna <crnana98@gmail.com>
2024-08-20 00:19:44 -0300

    fix json struct tag

--- models/models.go
+++ models/models.go
@@ -3,9 +3,9 @@ package models
 import "github.com/shopspring/decimal"
 
 type Product struct {
-	Id    string          `json:id`
-	Desc  string          `json:desc`
-	Price decimal.Decimal `json:price`
-	Cat   string          `json:cat`
-	Src   string          `json:src`
+	Id    string          `json:"id"`
+	Desc  string          `json:"desc"`
+	Price decimal.Decimal `json:"price"`
+	Cat   string          `json:"cat"`
+	Src   string          `json:"src"`
 }
