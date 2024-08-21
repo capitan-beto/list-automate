3d45dbfa524f6a1894943c405f0ca7ed05ad5f2a
capitan-beto <crnana98@gmail.com>
2024-08-21 10:51:11 -0700

    add db writing method

--- internal/tools/database.go
+++ internal/tools/database.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 
+	"cmd/api/main.go/models"
+
 	"github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
 )
@@ -33,4 +35,24 @@ func CreateConnection() (*sql.DB, error) {
 	db.SetMaxOpenConns(5)
 	fmt.Println("Connected!")
 	return db, err
+}
+
+func AddProduct(p *models.Product, db *sql.DB) error {
+	id := p.ID
+	desc := p.Desc
+	price := p.Price
+	subcat := p.Subcat
+	cat := p.Cat
+	src := p.Src
+	date := p.Date
+	alternID := p.AlternID
+
+	query := "REPLACE INTO products_db (id, item_desc, price, cat, src, date, subcat, altern_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
+	_, err := db.Exec(query, id, desc, price, cat, src, date, subcat, alternID)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("ID: " + id + " added!")
+	return nil
 }
